14af34150d0b70b7207acfc6a872c243c05e7523
Shivansh Raghav <shivanshraghav7@gmai.com>
2023-06-20 00:50:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function letterCombinations(input_digit) {
-  //Complete the function
+  let arr=[0,1,'abc','def','ghi','jkl','mno','pqrs','tuv','wxyz']
+	let res=[]
+	helper(arr,input_digit,res,0,0,"")
+	return arr
+}
+function helper(arr,input_digit,res,index1,index2,str){
+	if(str.length===input_digit.length){
+		arr.push(str)
+		return
+	}
+
+	let match = parseInt(input_digit[index1])
+	let ss = arr[match]
+	str=str+ss[index1]
+	helper(arr,input_digit,res,index1+1,index2,str)
+	str=str.substring(0,str.length())
+	helper(arr,input_digit,res,index1+1,index2,str)
 }
 
 module.exports = letterCombinations;
