adc42eedce18f58cbba41fcf93d6056445a7faa5
Shivansh Raghav <shivanshraghav7@gmai.com>
2023-06-20 01:03:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function helper(arr,input_digit,res,index1,str){
 	let match = parseInt(input_digit[index1])
 	let ss = arr[match]
 	for(let i=0;i<match.length;i++){
-		str=str+match[i]
+		str=str+ss[i]
 		helper(arr,input_digit,res,index1+1,str)
 		str=str.splice(0,str.length-1)
 	}
