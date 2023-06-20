606868349a9c8a860164dff4396f9f6e2aa16a01
Shivansh Raghav <shivanshraghav7@gmai.com>
2023-06-20 15:40:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let res=[]
 function letterCombinations(input_digit) {
   let arr=[0,1,'abc','def','ghi','jkl','mno','pqrs','tuv','wxyz']
-	helper(arr,input_digit,res,0,"")
+	helper(arr,input_digit,0,"")
 	return res.sort()
 }
-function helper(arr,input_digit,res,index1,str){
+function helper(arr,input_digit,index1,str){
 	if(str.length===input_digit.length){
 		res.push(str)
 		return
@@ -16,7 +16,7 @@ function helper(arr,input_digit,res,index1,str){
 	let ss = arr[match]
 	for(let i=0;i<ss.length;i++){
 		str=str+ss[i]
-		helper(arr,input_digit,res,index1+1,str)
+		helper(arr,input_digit,index1+1,str)
 		str=str.substring(0,str.length-1)
 	}
 }
